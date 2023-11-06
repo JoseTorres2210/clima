84103e9bcbdf8fe3679c0ff75a56b0d379c03f51
JoseTorres2210 <86307604+JoseTorres2210@users.noreply.github.com>
2023-11-06 10:30:03 -0500

    Update load_data.js

--- public/javascript/load_data.js
+++ public/javascript/load_data.js
@@ -155,14 +155,46 @@ let cargarFechaActual = () => {
   cargarOpenMeteo()
   
   let parseXML = (responseText) => {
-  
-    // Parsing XML
+
     const parser = new DOMParser();
     const xml = parser.parseFromString(responseText, "application/xml");
-  
-    console.log(xml)
-  
-  }
+
+
+    // Referencia al elemento `#forecastbody` del documento HTML
+
+    let forecastElement = document.querySelector("#forecastbody")
+    forecastElement.innerHTML = ''
+
+    // Procesamiento de los elementos con etiqueta `<time>` del objeto xml
+    let timeArr = xml.querySelectorAll("time")
+
+    timeArr.forEach(time => {
+        
+        let from = time.getAttribute("from").replace("T", " ")
+
+        let humidity = time.querySelector("humidity").getAttribute("value")
+        let windSpeed = time.querySelector("windSpeed").getAttribute("mps")
+        let precipitation = time.querySelector("precipitation").getAttribute("probability")
+        let pressure = time.querySelector("pressure").getAttribute("value")
+        let cloud = time.querySelector("clouds").getAttribute("value")
+
+        let template = `
+            <tr>
+                <td>${from}</td>
+                <td>${humidity}</td>
+                <td>${windSpeed}</td>
+                <td>${precipitation}</td>
+                <td>${pressure}</td>
+                <td>${cloud}</td>
+            </tr>
+        `
+
+        //Renderizando la plantilla en el elemento HTML
+        forecastElement.innerHTML += template;
+    })
+
+}
+
   
   //Callback
   let selectListener = async (event) => {
